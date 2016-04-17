60f47fa34fbca2376d20ad586cebf572070f1a90
Chongwei <lewgreat@gmail.com>
2016-04-17 23:44:11 +0800

    Starting MVC.

--- Calculator/Calculator/CaculatorBrain.swift
+++ Calculator/Calculator/CaculatorBrain.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+class CalculatorBrain{
+    
+    private enum Op {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+    }
+    
+    
+    private var opStack = [Op]()
+    
+    private var knownOps = [String:Op]()
+    
+    init() {
+        knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["−"] = Op.BinaryOperation("−") { $1 - $0}
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]){
+        if !ops.isEmpty{
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+                        
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        return nil
+    }
+    
+    func pushOperand(valueOfOperand: Double)  {
+        opStack.append(Op.Operand(valueOfOperand))
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = knownOps[symbol]{
+            opStack.append(operation)
+        }
+        
+    }
+    
+}
